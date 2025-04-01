5280f5dcf2dc9fa9c6870623c654e53a9f3421d7
siddhartasr10 <siddhartasr9@live.com>
2025-04-01 15:21:20 +0200

    Creado servicio ReactiveUserDetailService y ReactivePasswordService
    Se ha tenido que mejorar ExtUser y crear un UserRepository
    
    Antes se llamaba UserRep pero no es tan descriptivo

---
+++ SportWatch/src/main/java/com/reactive/SportWatch/services/UserService.java
@@ -0,0 +1,41 @@
+package com.reactive.SportWatch.services;
+
+import com.reactive.SportWatch.models.ExtUserDetails;
+import com.reactive.SportWatch.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsPasswordService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import reactor.core.publisher.Mono;
+
+
+
+public class UserService implements ReactiveUserDetailsService, ReactiveUserDetailsPasswordService {
+
+    private UserRepository users;
+
+    @Autowired
+    UserService(UserRepository UserRepository) {
+        this.users = UserRepository;
+    }
+
+    public Mono<UserDetails> findByUsername(String username) {
+        return Mono.just(users.findByUsername(username));
+    }
+
+    public Mono<ExtUserDetails> extFindByUsername(String username) {
+        return Mono.just(users.extFindByUsername(username));
+    }
+
+    public Mono<UserDetails> updatePassword(UserDetails user, String password) {
+        return Mono.just(users.updatePassword(user, password));
+    }
+
+    public Mono<ExtUserDetails> updatePassword(ExtUserDetails user, String password) {
+        return Mono.just(users.updatePassword(user, password));
+    }
+
+}
