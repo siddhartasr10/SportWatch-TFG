b741f6b0bc2aea1f7c3633a95bc5a1f4bf7a159e
siddhartasr10 <siddhartasr9@live.com>
2025-04-03 18:07:10 +0200

    Implementado un auth manager
    Fixes #2

--- SportWatch/src/main/java/com/reactive/SportWatch/config/SecurityConfig.java
+++ SportWatch/src/main/java/com/reactive/SportWatch/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.reactive.SportWatch.config;
 
+import com.reactive.SportWatch.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
 
 
 /** TODO Jira issue: KAN-2 and KAN-3
@@ -26,29 +31,36 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Bean
-    public MapReactiveUserDetailsService userDetailsService() {
-        UserDetails user = User.builder() // para usar este usuario primero hay que configurar el token csrf
-            .username("weakUser")
-            .password("{noop}weak")
-            .roles("USER")
-            .build();
+    private final UserService CustomUserDetailService;
 
-        return new MapReactiveUserDetailsService(user);
+    @Autowired
+    public SecurityConfig(UserService CustomUserDetailService) {
+        this.CustomUserDetailService = CustomUserDetailService;
     }
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-            .authorizeExchange(exchanges ->
-                               exchanges
-                    .anyExchange().authenticated()
-            )
+            .authorizeExchange(exchanges -> exchanges
+                               .anyExchange().authenticated())
             .httpBasic().and()
             .formLogin();
+
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
+
+    @Bean
+    public ReactiveAuthenticationManager authenticationProvider() { // Sad DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(passwordEncoder());
+        UserDetailsRepositoryReactiveAuthenticationManager authManager = new UserDetailsRepositoryReactiveAuthenticationManager(CustomUserDetailService);
+        authManager.setPasswordEncoder(passwordEncoder());
+        return authManager;
+    }
 
  // la configuracion implicita establece esto también pero si lo quieres modificar tienes que añadir esto.
 }
