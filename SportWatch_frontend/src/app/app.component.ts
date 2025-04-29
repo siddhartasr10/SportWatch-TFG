484da7c442db10dada20802ce51821e7322fddbf
siddhartasr10 <siddhartasr9@live.com>
2025-04-29 21:50:09 +0200

    Added landing and navbar components
    Also created routes for the landing and wildcard route
    and fonts were stated on tailwind (tailwind was installed also btw)

--- SportWatch_frontend/src/app/app.component.ts
+++ SportWatch_frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: 'app.component.html',
 })
+
 export class AppComponent {
   title = 'SportWatch_frontend';
 }
--- SportWatch_frontend/src/app/app.routes.ts
+++ SportWatch_frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'welcome', component: LandingComponent },
+    { path: '', redirectTo: '/welcome', pathMatch: 'full'},
+    // keep the wildcard as last route.
+    { path: '**', redirectTo: '/welcome'},
+];
