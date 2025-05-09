b55875ead877d1afd0c5f4413139201529e25245
siddhartasr10 <siddhartasr9@live.com>
2025-05-09 11:51:13 +0200

    Creada vista Login y enchufada al backend

--- SportWatch_frontend/src/app/views/login/login.component.ts
+++ SportWatch_frontend/src/app/views/login/login.component.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { Router, RouterLink } from '@angular/router';
+import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button'
+
+import { AuthService } from '../../shared/services/auth-service/auth-service.service';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component'
 
 @Component({
   selector: 'app-login',
-  imports: [],
+    imports: [NavbarComponent, RouterLink, ReactiveFormsModule, NgIf, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+    constructor(private authService: AuthService, private router : Router) {}
 
+    username : FormControl<string | null> = new FormControl('', [Validators.required, Validators.maxLength(50)]);
+    password : FormControl<string | null> = new FormControl('', [Validators.required, Validators.maxLength(72)]);
+
+    isFormInvalid : WritableSignal<boolean> = signal(true);
+
+    usernameErrMsg : WritableSignal<string> = signal('');
+    passwordErrMsg : WritableSignal<string> = signal('');
+
+    loginErr : WritableSignal<string> = signal('');
+    // Updates global form validation state, acting as a formGroup (i don't like short form groups).
+    updateFormValidity() : void {
+        (this.username.invalid || this.password.invalid)
+            ? this.isFormInvalid.set(true)
+            : this.isFormInvalid.set(false);
+    }
+
+
+    // Handles all error Msgs that come from validators.
+    // target: username || password
+    updateErrorMsg(target : string, control : FormControl) : void {
+        if (!control.errors) return;
+        let msg : string = "";
+        let firstErr = Object.keys(control.errors!)[0];
+
+        (firstErr === 'required')
+            ? msg = "¡Tienes que introducir un valor!"
+            : msg = "¡Campo demasiado largo!";
+
+        (target === "username")
+            ? this.usernameErrMsg.set(msg)
+            : this.passwordErrMsg.set(msg);
+
+    }
+
+
+    // Te registra y te lleva al feed, que por el momento no tengo implementado jajaj
+    handleLogin() : void {
+        this.authService.login(this.username.value!, this.password.value!).subscribe({
+            next: () => this.router.navigateByUrl("/feed"),
+            error: (body : HttpErrorResponse) => this.loginErr.set(body.error.message)
+        });
+    }
 }
