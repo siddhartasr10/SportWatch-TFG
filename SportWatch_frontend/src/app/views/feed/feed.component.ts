4d851c9453d3d3fbc8db86cf11f5ef5cd796b620
siddhartasr10 <siddhartasr9@live.com>
2025-05-21 08:17:32 +0200

    Terminado diseño feed
    Ademas importado plugin tailwind para retocar la scrollbar en caso de
    que haga falta.

--- SportWatch_frontend/src/app/views/feed/feed.component.ts
+++ SportWatch_frontend/src/app/views/feed/feed.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
-import { Route, RouterState } from '@angular/router';
+import { Component, WritableSignal, signal, } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { MatChipListbox, MatChipOption, MatChipsModule } from '@angular/material/chips';
 
 import { LoggedHeaderComponent } from '../../shared/components/logged-header/logged-header.component';
+import { VideoComponent } from '../../shared/components/video/video.component';
+import { Streaming } from '../../shared/interfaces/Streaming';
 
 @Component({
   selector: 'app-feed',
-  imports: [LoggedHeaderComponent],
+  imports: [LoggedHeaderComponent, MatChipsModule, VideoComponent],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.css'
 })
 export class FeedComponent {
-    constructor() {}
+    constructor(router : Router) {}
+
+    search : WritableSignal<string | null> = signal('');
+    selectedChips : WritableSignal<string[]> = signal([]);
+
+    videos : WritableSignal<Streaming[]> = signal([]);
+
+    // Me podría pasar los parámetros si quisiera del valor del .value del input si sacara el otro componente pero no sería escalable para funcionar en todos los componentes.
+    onSearch(searchQuery : string) {
+        this.search.set(searchQuery);
+
+        // Después del search habría un get a la api y luego el @for se encargaría del resto.
+
+    }
+
+    updateSelectedChips(chipList : MatChipListbox) {
+        let newChips = chipList.selected as MatChipOption[];
+
+        this.selectedChips.update( currentChips => {
+            // Vacio las chips que hayan.
+            currentChips.splice(0, currentChips.length);
+            for (let chip of newChips) {
+                if (!chip.selected) continue;
+                currentChips.push(chip.value);
+            }
+
+            return currentChips;
+        });
+
+        console.log("Selected Chips:", this.selectedChips());
+        // Me imagino que aquí filtraría la vista o llamaría a un filter con los videos que hay.
+    }
 
 }
--- SportWatch_frontend/src/app/views/landing/landing.component.ts
+++ SportWatch_frontend/src/app/views/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
