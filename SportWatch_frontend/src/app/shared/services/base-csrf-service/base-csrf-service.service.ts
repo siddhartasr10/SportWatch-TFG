116019266034c5d508f4480224a07ad19ca29c0e
siddhartasr10 <siddhartasr9@live.com>
2025-06-06 19:25:05 +0200

    Added some comments and minor corrections

--- SportWatch_frontend/src/app/shared/services/base-csrf-service/base-csrf-service.service.ts
+++ SportWatch_frontend/src/app/shared/services/base-csrf-service/base-csrf-service.service.ts
@@ -4,16 +4,15 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class BaseCsrfService {
-
     protected readonly apiUrl : string = 'http://localhost:4200/api';
     constructor(protected http: HttpClient) {
-        this.addCsrfTokenToCookies();
+        this.addCsrfTokenToCookies().subscribe();
     }
 
     // Gets CsrfToken and sets it on the existing cookies.
     // withCredentials only needed if different origin (im using proxy so no need), but just in case I don't need to add csrf token to header cause angular does this for me
-    public addCsrfTokenToCookies() : Observable<Object> {
-        return this.http.get(`${this.apiUrl}/csrf-token`, {withCredentials: true, responseType: 'text' });
+    public addCsrfTokenToCookies() : Observable<{msg: string}> {
+        return this.http.get<{msg: string}>(`${this.apiUrl}/csrf-token`, {withCredentials: true});
     }
 
     getXsrfToken() : string {
--- SportWatch_frontend/src/app/shared/services/user-service/user-service.service.ts
+++ SportWatch_frontend/src/app/shared/services/user-service/user-service.service.ts
@@ -16,6 +16,11 @@ export class UserService extends BaseCsrfService {
         super(http);
     }
 
+  /**
+   * Fetches extended user details for the given username.
+   * @param username The username of the user to retrieve.
+   * @returns Observable containing user details.
+   */
     getUserByUsername(username : string) : Observable<ExtUser> {
         return this.http.get<ExtUser>(`${this.apiUrl}/user/${username}`, {
             headers: new HttpHeaders({ 'X-XSRF-TOKEN': this.getXsrfToken() }),
@@ -25,6 +30,11 @@ export class UserService extends BaseCsrfService {
 
     }
 
+  /**
+   * Retrieves a list of followers for the given username.
+   * @param username The username whose followers are to be retrieved.
+   * @returns Observable containing an array of followers.
+   */
     getFollowersOfUsername(username : string) : Observable<FollowersStreamers[]> {
         return this.http.get<FollowersStreamers[]>(`${this.apiUrl}/followers/${username}`, {
             headers: new HttpHeaders({ 'X-XSRF-TOKEN': this.getXsrfToken() }),
@@ -34,6 +44,11 @@ export class UserService extends BaseCsrfService {
 
     }
 
+  /**
+   * Retrieves a list of suscribers for the given username.
+   * @param username The username whose suscribers are to be retrieved.
+   * @returns Observable containing an array of suscribers.
+   */
     getSuscribersOfUsername(username : string) : Observable<SuscribersStreamers[]> {
         return this.http.get<SuscribersStreamers[]>(`${this.apiUrl}/suscribers/${username}`, {
             headers: new HttpHeaders({ 'X-XSRF-TOKEN': this.getXsrfToken() }),
@@ -43,6 +58,11 @@ export class UserService extends BaseCsrfService {
 
     }
 
+  /**
+   * Retrieves a list of users the given username is following.
+   * @param username The username whose followings are to be retrieved.
+   * @returns Observable containing an array of followed users.
+   */
     getUsernameFollows(username : string) : Observable<FollowersStreamers[]> {
         return this.http.get<FollowersStreamers[]>(`${this.apiUrl}/follows/${username}`, {
             headers: new HttpHeaders({ 'X-XSRF-TOKEN': this.getXsrfToken() }),
@@ -52,6 +72,11 @@ export class UserService extends BaseCsrfService {
 
     }
 
+  /**
+   * Retrieves a list of streamers the user is suscribed to.
+   * @param username The username whose suscriptions are to be retrieved.
+   * @returns Observable containing an array of suscribed streamers.
+   */
     getUsernameSuscribed(username : string) : Observable<SuscribersStreamers[]> {
         return this.http.get<SuscribersStreamers[]>(`${this.apiUrl}/suscribed/${username}`, {
             headers: new HttpHeaders({ 'X-XSRF-TOKEN': this.getXsrfToken() }),
@@ -60,5 +85,82 @@ export class UserService extends BaseCsrfService {
         });
 
     }
+
+    /**
+    * Follows a streamer on behalf of a user.
+    * @param follower the username of the follower
+    * @param streamer the username of the streamer
+    */
+    followUser(follower: string, streamer: string): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/follow/${follower}/${streamer}`, null, {
+        headers: new HttpHeaders({ 'X-XSRF-TOKEN': this.getXsrfToken() }),
+        withCredentials: true
+    });
+    }
+
+    /**
+    * Subscribes a user to a streamer.
+    * @param suscriber the username of the suscriber
+    * @param streamer the username of the streamer
+    */
+    suscribeUser(suscriber: string, streamer: string): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/suscribe/${suscriber}/${streamer}`, null, {
+        headers: new HttpHeaders({ 'X-XSRF-TOKEN': this.getXsrfToken() }),
+        withCredentials: true
+    });
+    }
+
+    /**
+    * Unfollows a streamer on behalf of a user.
+    * @param follower the username of the follower
+    * @param streamer the username of the streamer
+    */
+    unfollowUser(follower: string, streamer: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/unfollow/${follower}/${streamer}`, {
+        headers: new HttpHeaders({ 'X-XSRF-TOKEN': this.getXsrfToken() }),
+        withCredentials: true
+    });
+    }
+
+    /**
+    * Unsubscribes a user from a streamer.
+    * @param suscriber the username of the suscriber
+    * @param streamer the username of the streamer
+    */
+    unsuscribeUser(suscriber: string, streamer: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/unsuscribe/${suscriber}/${streamer}`, {
+        headers: new HttpHeaders({ 'X-XSRF-TOKEN': this.getXsrfToken() }),
+        withCredentials: true
+    });
+    }
+
+    /**
+    * Checks if a user follows a streamer.
+    * @param follower the username of the follower
+    * @param streamer the username of the streamer
+    * @returns Observable<boolean>
+    */
+    checkFollows(follower: string, streamer: string): Observable<boolean> {
+    return this.http.get<boolean>(`${this.apiUrl}/follows/${follower}/${streamer}`, {
+        headers: new HttpHeaders({ 'X-XSRF-TOKEN': this.getXsrfToken() }),
+        withCredentials: true,
+        responseType: 'json'
+    });
+    }
+
+    /**
+    * Checks if a user is subscribed to a streamer.
+    * @param suscriber the username of the suscriber
+    * @param streamer the username of the streamer
+    * @returns Observable<boolean>
+    */
+    checkSuscribed(suscriber: string, streamer: string): Observable<boolean> {
+    return this.http.get<boolean>(`${this.apiUrl}/suscribes/${suscriber}/${streamer}`, {
+        headers: new HttpHeaders({ 'X-XSRF-TOKEN': this.getXsrfToken() }),
+        withCredentials: true,
+        responseType: 'json'
+    });
+    }
+
 
 }
