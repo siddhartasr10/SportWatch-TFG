56b05fe3c05e02612b2ffd0d74e95f2c1f7de49c
siddhartasr10 <siddhartasr9@live.com>
2025-06-06 19:23:09 +0200

    Added quick script to start all servers.
    When backend gets mixed with frontend maybe i will containerize it

---
+++ quickstart.sh
@@ -0,0 +1,5 @@
+#!/usr/bin sh
+
+$(gnome-terminal --tab -- sh -c "cd SportWatch; mvn clean spring-boot:run");
+$(gnome-terminal --tab -- sh -c "cd SportWatch_frontend; ng serve");
+$(gnome-terminal --tab -- sh -c "cd db; docker compose up -d");
