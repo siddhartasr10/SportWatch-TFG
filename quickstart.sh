5cb5ae743b94822c4578e00f695b6fc7e942ae3f
siddhartasr10 <siddhartasr9@live.com>
2025-09-12 16:01:08 +0200

    Minor: Adapted quickstart script.
    ng serve is not needed for the final release.

--- quickstart.sh
+++ quickstart.sh
@@ -2,5 +2,4 @@
 
 $(sudo systemctl start docker);
 $(gnome-terminal --tab -- sh -c "cd SportWatch; mvn clean spring-boot:run");
-$(gnome-terminal --tab -- sh -c "cd SportWatch_frontend; ng serve");
 $(gnome-terminal --tab -- sh -c "cd db; docker compose up -d");
